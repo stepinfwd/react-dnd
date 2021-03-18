bb406f7478c4ff7a8d545aa58fa33bde69070a89
kiran <kiran@dexlock.com>
2021-03-18 19:22:18 +0530

    resume card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
+import React, { useState, useEffect } from 'react'
+
 import SingleResume from "./SingleResume";
-import initialData from "./data"
+import {initialData} from "./data"
 function App() {
   const [resume, setResume] = useState(initialData.record);
 
   return (
     <div className="App">
-    {resume.map( (resume)=><SingleResume reusme={resume} />) }
-    </div>
+   {resume.map((item, index) => (
+        <SingleResume key={index} resume={item} />
+      ))}    </div>
   );
 }
 
